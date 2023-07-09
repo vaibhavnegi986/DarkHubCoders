29bac4d34a80b8b660cf02a78fef8cc3bbe93a2f
vaibhavnegi986 <vaibhav.negi.986@gmail.com>
2023-07-09 10:19:15 +0530

    First Change

---
+++ TeamBProject/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using TeamBProject_Modules;
+
+namespace TeamBProject
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<AppShell>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<StartupModule>();
+        }
+    }
+}
---
+++ TeamBProject_Modules/StartupModule.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeamBProject_Modules.ViewModels;
+using TeamBProject_Modules.Views;
+
+namespace TeamBProject_Modules
+{
+    public class StartupModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            //View Register Only
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("LoginRegion", typeof(LoginView));
+            regionManager.RegisterViewWithRegion("DashboardRegion", typeof(DashboardView));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //View Register Only
+            containerRegistry.RegisterForNavigation<LoginView, LoginViewViewModel>();
+            containerRegistry.RegisterForNavigation<DashboardView, DashboardViewViewModel>();
+        }
+    }
+}
